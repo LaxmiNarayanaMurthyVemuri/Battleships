23f362a127f497d484dcd61572b80f9f2cad454b
Kollurupranathi <kpranathi009@gmail.com>
2021-09-26 18:18:27 +0530

    create grid,ship

--- battleship.py
+++ battleship.py
@@ -62,7 +62,14 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    x = []
+    for i in range(rows):
+        s= []
+        for j in range(cols):
+            s.append(1)
+        x.append(s)
+    return x
+
 
 
 '''
@@ -71,7 +78,13 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    row = random.randint(1,8)
+    col =random.randint(1,8)
+    horizantal=random.randint(0,1)
+    if not horizantal:
+        return ([[row-1,col],[row,col],[row+1,col]])
+    else:
+        return ([[row,col-1],[row,col],[row,col+1]])
 
 
 '''
@@ -268,6 +281,6 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-
+    test.testCreateShip()
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    #runSimulation(500, 500)
