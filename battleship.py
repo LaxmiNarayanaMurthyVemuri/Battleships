53410da5e2837e40c09c68ba626e914d84a28548
Kollurupranathi <kpranathi009@gmail.com>
2021-09-29 14:56:46 +0530

    isVertical

--- battleship.py
+++ battleship.py
@@ -41,6 +41,8 @@ Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
 Returns: None
 '''
 def makeView(data, userCanvas, compCanvas):
+    userCanvas= drawGrid(data, userCanvas, data["usergrid"], True)
+    compCanvas = drawGrid(data, compCanvas, data["computergrid"], True)
     return
 
 
@@ -149,16 +151,28 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isVertical(ship):
-    return
+    temp =[]
+    for i in range(len(ship)):
+        temp.append(ship[i][0])
+    temp.sort()
+    for i in range(len(ship)-1):
+        if ship[i][1]!=ship[i+1][1] or temp[i]!=temp[i+1]-1:
+            return False
+    return True
 
-
+  
 '''
 isHorizontal(ship)
 Parameters: 2D list of ints
 Returns: bool
 '''
 def isHorizontal(ship):
-    return
+    ship = createShip(ship)
+    if ship == 0:
+      return True
+    else: 
+      return False
+   
 
 
 '''
@@ -309,6 +323,6 @@ def runSimulation(w, h):
 # This code runs the test cases to check your work
 if __name__ == "__main__":
     # test.testCheckShip()
-    # test.testMakeModel()
+     test.testIsVertical()
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    # runSimulation(500, 500)
