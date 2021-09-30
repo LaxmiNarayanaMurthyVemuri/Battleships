329b57d95d68743aad53e996fc90583b28afbe94
Kollurupranathi <kpranathi009@gmail.com>
2021-09-30 09:50:52 +0530

    isVertical done

--- battleship.py
+++ battleship.py
@@ -153,12 +153,11 @@ Returns: bool
 def isVertical(ship):
     temp =[]
     for i in range(len(ship)):
-        temp.append(ship[i][0])
+       temp.append(ship[i][0])
     temp.sort()
-    for i in range(len(ship)-1):
-        if ship[i][1]!=ship[i+1][1] or temp[i]!=temp[i+1]-1:
-            return False
-    return True
+    if ship[0][1]==ship[1][1]==ship[2][1] and temp[0]==temp[1]-1==temp[2]-2:
+       return True
+    return False
 
   
 '''
