4a5298fc1d7a6860e1a3416ddbd3a02b78cf6c32
CH.Nithin Chakravarthy <94435704+Chakravarthy738@users.noreply.github.com>
2021-12-16 11:43:56 +0530

    createShip

--- battleship.py
+++ battleship.py
@@ -77,7 +77,15 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    r=random.randint(1,8)
+    c=random.randint(1,8)
+    b=random.randint(0,1)
+    if b == 0:
+        ship=[[r-1,c],[r,c],[r+1,c]]
+    else:
+        ship=[[r,c-1],[r,c],[r,c+1]]
+    return ship
+print(createShip());
 
 
 '''
@@ -274,7 +282,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testEmptyGrid()
+    test.createShip()
 
     ## Finally, run the simulation to test it manually ##
     # runSimulation(500, 500)
